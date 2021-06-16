2bccc4eb23cab548b68982ffc66f70d94b6c539f
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-06-16 15:24:25 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e82b180cbfc1cf48e7be432d62fdfb56'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb147a40-1425-49ee-ad74-d3a2c29c2168'
+};
