dd4cb1e72586cc1b065060b20dc5ed98cbdb9699
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-06-27 20:24:25 +0000

    update

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.q1yoz.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'website-cms'),
-        username: env('DATABASE_USERNAME', 'gcu123'),
-        password: env('DATABASE_PASSWORD', 'Inumluceant20'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
